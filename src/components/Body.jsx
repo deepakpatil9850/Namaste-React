08a004ee312dd4fc56a87596c4ec4a44bcddc1a7
Deepak Patil <deepakpatil0999@gmail.com>
2024-01-16 02:09:52 +0530

    shimmer and filter added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,45 +1,42 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
-import restroData from "../utils/restroData";
+import Shimmer from "./Shimmer";
+
 
 const Body = () =>{
-
-
-    const [resList, setResList] = useState(restroData);
+    const [resList, setResList] = useState([]);
 
     useEffect(()=>{
         fetchData();
-    },[]);
+        },[]);
 
     const fetchData = async ()=>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const dataJson = await data.json();
 
-        //Optional chaining
-            //setResList(data?.cards?.gridElements?.infoWithStyle?.restaurents.info);
-        setResList(dataJson?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants) //cards[1].card.card.gridElements.infoWithStyle.restaurants);
-        console.log(dataJson)
+        setResList(dataJson?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants) 
         }
-      //[1].info;
+      
+        const topRated = ()=>{
+            const filterlist = resList.filter(res => res?.info?.avgRating >= 4.3)
+            setResList(filterlist)    
+        }
 
-
-    return (
+    return (resList.length ===0) ? ( <Shimmer />) : (
         <div className="res-container">
 
             <div>
                 <input type="text" />
                 <button>Search</button>
+                <button onClick={topRated}>Top Rated</button>
             </div>
-
+           
             <div className="product-list">
-                 {
-                  resList.map((res)=>{
-                      return  <Card key={ res?.info?.id} resData={res}/>
-                     
+                {
+                  resList.map((res)=>{ return  <Card key={ res?.info?.id} resData={res}/>
                   })
                 }
-                <Card></Card>
-            </div>
+            </div>      
         </div>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,10 @@
-import { useEffect,useState } from "react";
 import { cloudLink } from "../utils/restroData";
 
 const Card = (props) =>{
 
-  
     return(
         <div className="card">
-                <img alt="food-img" src={cloudLink+props?.resData?.info.cloudinaryImageId } className="cart-img" />
+                <img alt="food-img" src={cloudLink+props?.resData?.info?.cloudinaryImageId } className="cart-img" />
                 <h3>{props?.resData?.info?.name}</h3>
                 <h5>{props?.resData?.info?.cuisines.map(cusin=>cusin).join(" ")}</h5>
                 <h5>{props?.resData?.info?.costForTwo}</h5>
